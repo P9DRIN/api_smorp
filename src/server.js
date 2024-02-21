d024bcda5f587219b15b90d2afb5568e41be5431
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-02-21 16:30:04 -0300

    cors update

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 db.connect()
 app.use(routes)
+app.use(cors({ origin: 'https://api-smorp.vercel.app'}))
 
 
 app.listen(port, () => {console.log(`🚀 Backend started at http://localhost:${port} `)})
